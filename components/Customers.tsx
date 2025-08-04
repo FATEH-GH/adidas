a9562aa8f5a0dea6d99e3d08950cdc87cbd3cf53
FATEH-GH <fatehghribi27@gmail.com>
2025-08-04 09:15:32 +0100

    fixed the mobile version for the testimonials

--- components/Customers.tsx
+++ components/Customers.tsx
@@ -4,13 +4,17 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-card";
 const Customers = () => {
   return (
     <section className="bg-gray-400 p-8  ">
-      <div className="flex items-center justify-center">
-        <InfiniteMovingCards direction="up" speed="slow" />
-        <div className="flex flex-col items-center justufy-center">
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+        <InfiniteMovingCards
+          direction="up"
+          speed="slow"
+          className="hidden lg:block "
+        />
+        <div className="flex flex-col items-center justufy-center mx-auto">
           <h3 className="text-[40px] font-bold text-center ">
             What Our <span className="text-orange-500">Customers</span> Say?
           </h3>
-          <p className="text-white text-[22px] text-center max-w-lg ">
+          <p className="text-white text-[22px] text-center max-w-lg mx-auto">
             Hear genuine stories from our satisfied customers about their
             exceptional experiences with us.
           </p>
