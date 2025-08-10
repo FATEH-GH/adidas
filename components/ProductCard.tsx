58a15d18fcc20c60592f415a1a7b6d7236d3f86b
FATEH-GH <fatehghribi27@gmail.com>
2025-08-10 17:13:38 +0100

    fixed some design issues

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,18 +5,20 @@ import { BackgroundGradient } from "./ui/background-gradient";
 
 const Product = ({ image, price, name }: ProductCardProps) => {
   return (
-    <div className="flex flex-col flex-1 gap-2">
-      <BackgroundGradient className="flex flex-col  rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+    <div className="flex flex-col flex-1 gap-2 px-2">
+      <BackgroundGradient className="flex flex-col  rounded-[22px] max-w-sm p-4  bg-white dark:bg-zinc-900">
         <figure className="flex items-center justify-center">
           <Image src={image} alt="shoeImage" height={300} width={300} />
         </figure>
-        <div className="card-body">
+        <div className=" border-2">
           <div className="flex items-center justify-start gap-2 my-2">
             <Image src="/star.svg" height={25} width={25} alt="star" />
             <p className="text-gray-500 text-[18px] ">(4.5)</p>
           </div>
 
-          <h5 className="font-semibold md:text-xl mb-2 text-[20px]">{name}</h5>
+          <h5 className="font-semibold md:text-lg lg:text-xl mb-2 text-[18px] border-2 border-white">
+            {name}
+          </h5>
           <p className="mb-4 text-red-400 text-[22px] font-semibold">
             ${price}
           </p>
